a8f93febf17a3c6ddf9ce101b6218b10f9743dfa
Martin Miguel Custidiano Secchi <martinsecchi@gmail.com>
2026-02-23 16:22:53 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from mean_var_std import calculate
+from unittest import main
+
+# Prueba rápida
+print(calculate([0, 1, 2, 3, 4, 5, 6, 7, 8]))
+
+# Ejecutar tests automáticos
+main(module='test_module', exit=False)
